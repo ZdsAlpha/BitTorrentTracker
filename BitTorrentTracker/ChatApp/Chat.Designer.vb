0b4342525f58f49bacbbbf6205a9bf5e22d41acb
ZDS Alpha <abzsoft999@hotmail.com>
2016-09-09 09:43:49 +0500

    tbca_0

--- BitTorrentTracker/ChatApp/Chat.Designer.vb
+++ BitTorrentTracker/ChatApp/Chat.Designer.vb
@@ -22,21 +22,72 @@ Partial Class Chat
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Trackers = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Trackers:"
+        '
+        'Trackers
+        '
+        Me.Trackers.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Trackers.Location = New System.Drawing.Point(15, 25)
+        Me.Trackers.Multiline = True
+        Me.Trackers.Name = "Trackers"
+        Me.Trackers.Size = New System.Drawing.Size(565, 95)
+        Me.Trackers.TabIndex = 1
+        Me.Trackers.Text = "tracker.coppersurfer.tk:6969" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "tracker.mg64.net:2710" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "tracker.leechers-paradise.or" &
+    "g:6969" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "tracker.sktorrent.net:6969"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 129)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Channel:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(73, 126)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(507, 20)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "__tbca_default_channel"
         '
         'Chat
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(412, 327)
+        Me.ClientSize = New System.Drawing.Size(592, 428)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Trackers)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Name = "Chat"
         Me.Opacity = 0.95R
         Me.ShowIcon = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Tracker Based Chat App"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Trackers As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
 End Class
