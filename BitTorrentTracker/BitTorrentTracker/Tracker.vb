41bcc13b02429abba5c7fdd1697a6ad953c446b8
ZDS Alpha <abzsoft999@hotmail.com>
2016-09-09 08:23:29 +0500

    edited

--- BitTorrentTracker/BitTorrentTracker/Tracker.vb
+++ BitTorrentTracker/BitTorrentTracker/Tracker.vb
@@ -1,33 +1,61 @@
-﻿Imports System.Net
+﻿Imports System.ComponentModel
+Imports System.Net
 Imports System.Net.Sockets
 Imports MiscUtil.IO
 
 Public Class Tracker
-    Private client As New UdpClient
-    Private bitconverter As New MiscUtil.Conversion.BigEndianBitConverter
-    Private random As New Random
-    Private EndPoint As IPEndPoint
-    Private connection_id As Long = &H41727101980L
+    Implements IDisposable
+    Private Shared bitconverter As New MiscUtil.Conversion.BigEndianBitConverter
+    Private Shared random As New Random
+    Private _client As New UdpClient
+    Private _endpoint As IPEndPoint
+    Private _connectionid As Long = &H41727101980L
+    Public ReadOnly Property IsDisposed As Boolean
+        Get
+            Return _client Is Nothing
+        End Get
+    End Property
+    Public ReadOnly Property IsConnected As Boolean
+        Get
+            If _endpoint IsNot Nothing AndAlso _connectionid <> &H41727101980L Then Return True
+            Return False
+        End Get
+    End Property
+    Public ReadOnly Property Client As UdpClient
+        Get
+            Return _client
+        End Get
+    End Property
+    Public ReadOnly Property EndPoint As IPEndPoint
+        Get
+            Return _endpoint
+        End Get
+    End Property
+    Public ReadOnly Property ConnectionId As Long
+        Get
+            Return _connectionid
+        End Get
+    End Property
     Public WriteOnly Property Timeout As Integer
         Set(value As Integer)
-            client.Client.SendTimeout = value
-            client.Client.ReceiveTimeout = value
+            _client.Client.SendTimeout = value
+            _client.Client.ReceiveTimeout = value
         End Set
     End Property
     Public Sub Connect(EndPoint As IPEndPoint)
         Dim transaction_id As Integer = random.Next(65535)
         Dim request As New IO.MemoryStream
         Dim writer As New EndianBinaryWriter(bitconverter, request)
-        writer.Write(connection_id)
+        writer.Write(_connectionid)
         writer.Write(0)
         writer.Write(transaction_id)
         Dim rawrequest As Byte() = request.ToArray
         writer.Dispose()
         request.Dispose()
-        If client.Send(rawrequest, rawrequest.Length, EndPoint) <> rawrequest.Length Then Throw New Exception("Packet was not sent properly.")
+        If _client.Send(rawrequest, rawrequest.Length, EndPoint) <> rawrequest.Length Then Throw New Exception("Packet was not sent properly.")
         Dim source As IPEndPoint = Nothing
 Retry:
-        Dim rawresponse As Byte() = client.Receive(source)
+        Dim rawresponse As Byte() = _client.Receive(source)
         If source Is Nothing OrElse rawresponse Is Nothing Then Throw New Exception("Failed to get response from server.")
         If Not EndPoint.Equals(source) Then
             source = Nothing
@@ -38,8 +66,8 @@ Retry:
         Dim reader As New EndianBinaryReader(bitconverter, response)
         If reader.ReadInt32 <> 0 Then Throw New Exception("Invalid return parameter 'action'.")
         If reader.ReadInt32 <> transaction_id Then Throw New Exception("Invalid transaction id or packet is corrupted.")
-        connection_id = reader.ReadInt64
-        Me.EndPoint = EndPoint
+        _connectionid = reader.ReadInt64
+        Me._endpoint = EndPoint
         reader.Dispose()
         response.Dispose()
     End Sub
@@ -51,15 +79,15 @@ Retry:
     End Sub
     Public Function Announce(Hash As Byte(), PeerId As Byte(), Optional Downloaded As Long = 0, Optional Remaining As Long = 0, Optional Uploaded As Long = 0, Optional [Event] As EventTypeFilter = EventTypeFilter.Started, Optional IPAddress As IPAddress = Nothing, Optional Count As Integer = -1, Optional Port As Short = 0) As AnnounceResult
         If IPAddress Is Nothing Then IPAddress = New IPAddress(0)
-        If client Is Nothing Then Throw New Exception("Tracker is disposed.")
-        If EndPoint Is Nothing Or connection_id = &H41727101980L Then Throw New Exception("Not connected to tracker.")
+        If _client Is Nothing Then Throw New Exception("Tracker is disposed.")
+        If _endpoint Is Nothing Or _connectionid = &H41727101980L Then Throw New Exception("Not connected to tracker.")
         If Hash.Length <> 20 Then Throw New Exception("Invalid hash.")
         If PeerId.Length <> 20 Then Throw New Exception("Invalid PeerId.")
         Dim transaction_id As Integer = random.Next(65535)
         Dim key As Integer = random.Next(65535)
         Dim request As New IO.MemoryStream
         Dim writer As New EndianBinaryWriter(bitconverter, request)
-        writer.Write(connection_id)
+        writer.Write(_connectionid)
         writer.Write(1)
         writer.Write(transaction_id)
         writer.Write(Hash)
@@ -75,12 +103,12 @@ Retry:
         Dim rawrequest As Byte() = request.ToArray
         writer.Dispose()
         request.Dispose()
-        If client.Send(rawrequest, rawrequest.Length, EndPoint) <> rawrequest.Length Then Throw New Exception("")
+        If _client.Send(rawrequest, rawrequest.Length, _endpoint) <> rawrequest.Length Then Throw New Exception("")
         Dim source As IPEndPoint = Nothing
 Retry:
-        Dim rawresponse As Byte() = client.Receive(source)
+        Dim rawresponse As Byte() = _client.Receive(source)
         If source Is Nothing OrElse rawresponse Is Nothing Then Throw New Exception("Failed to get response from server.")
-        If Not EndPoint.Equals(source) Then
+        If Not _endpoint.Equals(source) Then
             source = Nothing
             GoTo Retry
         End If
@@ -100,6 +128,11 @@ Retry:
         End While
         Return New AnnounceResult With {.Interval = interval, .Leechers = leechers, .Seeders = seeders, .Clients = clients.ToArray}
     End Function
+    Public Sub Dispose() Implements IDisposable.Dispose
+        _client.Close()
+        _client = Nothing
+        _connectionid = &H41727101980L
+    End Sub
     Public Structure AnnounceResult
         Public Interval As Integer
         Public Leechers As Integer
