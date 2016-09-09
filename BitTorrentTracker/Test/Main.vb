98e80060697f31e19632219c1d4c75dba6151861
ZDS Alpha <abzsoft999@hotmail.com>
2016-09-09 08:31:22 +0500

    added test

---
+++ BitTorrentTracker/Test/Main.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Net
+Imports BitTorrentTracker
+
+Module Main
+    Sub Main()
+        Dim tracker As New Tracker
+        tracker.Connect("tracker.opentrackr.org", 1337)
+        Dim name(19) As Byte
+        Dim random As New Random
+        random.NextBytes(name)
+        Dim result = tracker.Announce({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20}, name,,,,,,, 8080)
+        Stop
+    End Sub
+End Module
